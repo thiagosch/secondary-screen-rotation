9ef64fa9f14030eee0df41bf5a115f06b5465807
santiago <thiagosch.a@gmail.com>
2022-06-08 17:40:01 -0300

    Add files via upload

---
+++ rotate screen.py
@@ -0,0 +1,69 @@
+import rotatescreen
+import keyboard
+
+from infi.systray import SysTrayIcon
+states = {"primarystate": 0, "secondarystate": 270}
+primarystate = 0
+secondarystate = 270
+
+
+def rotatesecondary(resetvalue=False):
+    if(rotatescreen.get_secondary_displays()[0].current_orientation == states["primarystate"]):
+        rotation = states["secondarystate"]
+    else:
+        rotation = states["primarystate"]
+    if resetvalue:
+        rotation = states["primarystate"]
+    settrayIconTo(rotation)
+    return screen.rotate_to(rotation)
+
+
+def settrayIconTo(value):
+    match value:
+        case 0:
+            Icon = "landscape.ico"
+        case 90:
+            Icon = "portrait.ico"
+        case 180:
+            Icon = "landscapeF.ico"
+        case 270:
+            Icon = "portraitF.ico"
+    systray.update(icon=Icon)
+
+
+def say_hello(systray):
+    rotatesecondary(True)
+    return
+
+
+def setrotationparams(state):
+    states["primarystate"] = state[0]
+    states["secondarystate"] = state[1]
+
+
+def test(systray):
+    print(states)
+
+
+menu_options = (
+    ("reset rotation", None, say_hello),
+    ("set rotation 0-270", None, lambda systray:  setrotationparams([0, 270])),
+    ("set rotation 0-90", None, lambda systray:  setrotationparams([0, 90])),
+    ("set rotation 0-180", None, lambda systray:  setrotationparams([0, 180])),
+    ("set rotation 90-270", None,
+     lambda systray:  setrotationparams([90, 270])),
+    ("set secondary as primary", None, lambda systray: setrotationparams([states["secondarystate"], states["primarystate"]])),)
+
+
+systray = SysTrayIcon("icon.ico", "Screen rotation", menu_options)
+settrayIconTo(rotatescreen.get_secondary_displays()[0].current_orientation)
+systray.start()
+
+
+screen = rotatescreen.get_secondary_displays()[0]
+# rotatescreen.get_secondary_displays()[0].rotate_to(90)
+
+
+keyboard.add_hotkey("ctrl+alt+j", rotatesecondary)
+
+keyboard.wait()
