c1d33d06a7b5dcf334c4e4f2a0d9b77a0fde47a6
santiago <thiagosch.a@gmail.com>
2022-06-12 16:10:06 -0300

    some changes in code structure
    changes to facilitate the use of multiple files.

--- rotate screen.py
+++ rotate screen.py
@@ -3,6 +3,8 @@ import keyboard
 import os
 import sys
 from infi.systray import SysTrayIcon
+
+
 states = {"primarystate": 0, "secondarystate": 270}
 primarystate = 0
 secondarystate = 270
@@ -31,7 +33,6 @@ def rotatesecondary(resetvalue=False):
 
 
 def settrayIconTo(value):
-    print(resource_path("images/landscape.ico"))
     if(value == 0):
         Icon = resource_path("images/landscape.ico")
     elif value == 90:
@@ -48,14 +49,19 @@ def setrotationparams(state):
     states["secondarystate"] = state[1]
 
 
+def set_shortcut():
+    return
+
+
 menu_options = (
-    ("reset rotation", None, lambda systray: rotatesecondary(True)),
-    ("set rotation 0-270", None, lambda systray:  setrotationparams([0, 270])),
-    ("set rotation 0-90", None, lambda systray:  setrotationparams([0, 90])),
-    ("set rotation 0-180", None, lambda systray:  setrotationparams([0, 180])),
-    ("set rotation 90-270", None,
+    ("Reset rotation", None, lambda systray: rotatesecondary(True)),
+    ("Set shortcut", None, lambda systray: set_shortcut),
+    ("Set rotation 0-270", None, lambda systray:  setrotationparams([0, 270])),
+    ("Set rotation 0-90", None, lambda systray:  setrotationparams([0, 90])),
+    ("Set rotation 0-180", None, lambda systray:  setrotationparams([0, 180])),
+    ("Set rotation 90-270", None,
      lambda systray:  setrotationparams([90, 270])),
-    ("set secondary as primary", None, lambda systray: setrotationparams([states["secondarystate"], states["primarystate"]])),)
+    ("Set secondary as primary", None, lambda systray: setrotationparams([states["secondarystate"], states["primarystate"]])),)
 
 
 def on_quit_callback(systray):
@@ -65,9 +71,14 @@ def on_quit_callback(systray):
 systray = SysTrayIcon("icon.ico", "Screen rotation",
                       menu_options, on_quit=on_quit_callback)
 settrayIconTo(rotatescreen.get_secondary_displays()[0].current_orientation)
-systray.start()
+
 screen = rotatescreen.get_secondary_displays()[0]
 
 
-keyboard.add_hotkey("ctrl+alt+j", rotatesecondary)
-keyboard.wait()
+def run_tray_app():
+    systray.start()
+    keyboard.add_hotkey("ctrl+alt+j", rotatesecondary)
+    keyboard.wait()
+
+
+run_tray_app()
